b5863c3d372d2a8aa2be740c7fac160601b38a75
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-07-07 15:44:52 +0800

    feat(navigation): register staff document entry and analysis screens in navigator

--- src/navigation/StaffNavigator.jsx
+++ src/navigation/StaffNavigator.jsx
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import StaffHomeScreen from "../screens/staff/StaffHomeScreen";
+import StaffDocumentEntry from "../screens/staff/StaffDocumentEntry";
+import StaffAnalysisResults from "../screens/staff/StaffAnalysisResults";
 
 const Stack = createNativeStackNavigator();
 
@@ -7,6 +9,14 @@ export default function StaffNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="StaffHome" component={StaffHomeScreen} />
+      <Stack.Screen
+        name="StaffDocumentEntry"
+        component={StaffDocumentEntry}
+      />
+      <Stack.Screen
+        name="StaffAnalysisResults"
+        component={StaffAnalysisResults}
+      />
     </Stack.Navigator>
   );
 }
